06823ea10a3689eea72baa66e108e9c84c3e66eb
Brad Sickles <brad.sickles@gmail.com>
2025-02-21 19:21:21 -0500

    Revert fix

--- ca.tf
+++ ca.tf
@@ -1,8 +1,8 @@
-data "ns_app_connection" "ca" {
+data "ns_connection" "ca" {
   name     = "ca"
   contract = "datastore/gcp/ca:internal"
 }
 
 locals {
-  ca_cert_pem = data.ns_app_connection.ca.outputs.root_cert_pem
+  ca_cert_pem = data.ns_connection.ca.outputs.root_cert_pem
 }
